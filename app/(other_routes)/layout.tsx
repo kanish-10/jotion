57adae3388a8e2e7f85e5a83b6244f90d158a01a
Kanish Chheda <kanishchheda@gmail.com>
2023-10-13 14:48:04 +0530

    sidebar

---
+++ app/(other_routes)/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { redirect } from "next/navigation";
+import Navigation from "@/app/(other_routes)/_components/navigation";
+
+export default function OtherLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  );
+}
