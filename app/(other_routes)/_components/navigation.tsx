0c1ae1d502752cc82ea14d8fa7c9777a5a075c8d
Kanish Chheda <kanishchheda@gmail.com>
2023-10-15 01:57:56 +0530

    trash box

--- app/(other_routes)/_components/navigation.tsx
+++ app/(other_routes)/_components/navigation.tsx
@@ -3,9 +3,11 @@
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from "lucide-react";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -17,6 +19,12 @@ import { api } from "@/convex/_generated/api";
 import Item from "@/app/(other_routes)/_components/item";
 import { toast } from "sonner";
 import { DocumentList } from "@/app/(other_routes)/_components/document-list";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import TrashBox from "@/app/(other_routes)/_components/trash-box";
 
 export default function Navigation() {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -135,6 +143,18 @@ export default function Navigation() {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label={"Add a page"} />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              side={isMobile ? "bottom" : "right"}
+              className="p-0 w-72"
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
