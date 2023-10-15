9b23764b2b91648503165a9e1ad1285c59d3680f
Kanish Chheda <kanishchheda@gmail.com>
2023-10-15 18:38:59 +0530

    toolbar component

--- app/(other_routes)/(routes)/documents/[documentId]/page.tsx
+++ app/(other_routes)/(routes)/documents/[documentId]/page.tsx
@@ -1,3 +1,35 @@
-export default function DocumentPage() {
-  return <div>Document Page</div>;
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Toolbar from "@/components/tool-bar";
+
+interface DocumentIdProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+export default function DocumentPage({ params }: DocumentIdProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 }
